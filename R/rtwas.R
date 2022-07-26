64c4680a032380fe4a65910462ae8ea4b4263842
Alexander Ploner <alexander.ploner@ki.se>
2022-07-26 17:53:05 +0200

    Extended gene prediction function to return matched SNPs
    Revised the original function for predicting genetically determined
    expression to also return a logical vector to indicate which SNPs in
    the reference data carry EQTL information (used for identifying loci
    downstream). Not pretty, but both results need iteration over the
    on-disk files with EQTL information, so probably better for all.

--- R/rtwas.R
+++ R/rtwas.R
@@ -1,8 +1,8 @@
-#' Predict expression on genetic reference data
+#' Map EQTL gene data to reference data
 #'
-#' Given a list of genes and the expression weights for their eQTLs,
-#' calculate the matrix of predicted gene expression values on a
-#' genetic reference data set.
+#' Given a list of genes and the weights for their EQTL-SNPs, this function maps
+#' the EQTL-SNPs to SNP-level reference data, and predicts all genetic expression
+#' components in the expression data.
 #'
 #' @param genelist A data frame with information on the genes of interest,
 #'                 including the path to the data file containing the expression
@@ -10,13 +10,24 @@
 #' @param refdata Genetic reference data, as a list with PLINK-style components;
 #'                typically output from `read_refdata`
 #'
-#' @returns A matrix with subjects in the reference data as rows and genes as
-#' columns, containing for each subject / gene the predicted expression level
-#' (scaled across ubjects).
+#' @returns A list with two components:
+#'
+#'     * `refsnps_keep`: a logical vector that indicates for each SNP in the
+#'                       reference data whether it has a weight associated in
+#'                       the EQTL data.
+#'     * `pred_expression`: a numerical matrix with the predicted gene expression
+#'                          component for each gene (as columns) for all subjects
+#'                          in the reference data (as rows).
+#'
+#' @details This is a bit of a mixed function, in that the two result components
+#' are only loosely related, and would logically be calculated in separate
+#' functions. However, calculation of both requires iterating over the per-gene
+#' expression weights, which are stored as files, so that would not be very
+#' efficient.
 #'
 #' @seealso [read_genelist()], [read_refdata()]
 #' @export
-predict_expression <- function(genelist, refdata)
+eqtl_to_refdat <- function(genelist, refdata)
 {
   nsnps_ref <- nrow(refdata$bim)
   nsubj_ref <- nrow(refdata$bed)
@@ -39,12 +50,15 @@ predict_expression <- function(genelist, refdata)
     ## Match up the SNPs and weights, iow only keep SNPs relevant
     ## for current gene prediction
     ## FIXME: use variable names instead of col numbers where possible
+    ## FIXME: what if weight SNPs are not part of reference? That would
+    ## affect predicted expression? Should that not at least be reported?
     m      <- match( ww$snps[,2] , refdata$bim[,2] )
     m.keep <- !is.na(m)
     ## Reduce the weight data to match
     ww$snps <- ww$snps[m.keep, ]
     ww$wgt.matrix = ww$wgt.matrix[m.keep, ]
     refsnps_keep[ m[m.keep] ] = TRUE
+
     ## Reduce the reference data to match
     cur.genos <- refdata$bed[, m[m.keep] ]
     cur.bim   <- refdata$bim[m[m.keep], ]
@@ -58,8 +72,19 @@ predict_expression <- function(genelist, refdata)
     mod <- which(colnames(ww$wgt.matrix) == genelist$MODEL[i])
     predmat[, i] <- cur.genos %*% ww$wgt.matrix[ , mod ]
   }
-  ## Scale gene expression across su
+  ## Scale gene expression across subjects / within genes
   predmat <- scale( predmat )
 
-  predmat
+  list(refsnps_keep = refsnps_keep, pred_expression = predmat)
 }
+
+#' Condition per-SNP GWAS statistics on per-gene TWAS statistics
+#'
+#' Given GWAS summary statistics for a chromosome and a corresponding set of
+#' TWAS test statistics, this function will calculate adjusted versions of the
+#' summary statistics, conditional on the observed TWAS statistics.
+#'
+#' @param
+#' @param opts List of options used for extracting default values for unspecified
+#'             arguments
+condTWAS_snp <- function(genelist, opts = opts_rtwas$get())
