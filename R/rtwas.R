5b93d59242f8e20763cdf78ea76dac9718c2b8ca
Alexander Ploner <alexander.ploner@ki.se>
2022-07-27 13:07:58 +0200

    Added calculation of TWAS-conditional SNP stats
    This function takes EQTL data, reference data, and summary
    statistics, plus some derived quantities lile loci, to
    adjust the observed summary statistics for the SNPs for
    the observed TWAS-statistics (per locus).

--- R/rtwas.R
+++ R/rtwas.R
@@ -84,7 +84,219 @@ eqtl_to_refdat <- function(genelist, refdata)
 #' TWAS test statistics, this function will calculate adjusted versions of the
 #' summary statistics, conditional on the observed TWAS statistics.
 #'
-#' @param
+#' @param summarystats
+#' @param genelist
+#' @param refdata
+#' @param pred_ge
+#' @param pred_ge_corr
+#' @param cons_loci
+#' @param lambda
 #' @param opts List of options used for extracting default values for unspecified
 #'             arguments
-condTWAS_snp <- function(genelist, opts = opts_rtwas$get())
+condTWAS_snp <- function(summarystats, genelist, refdata, pred_ge, pred_ge_corr,
+                         cons_loci, lambda = 0, opts = opts_rtwas$get())
+{
+  nloci  <- nrow(cons_loci)
+  ngenes <- nrow(genelist)
+  nsubj  <- nrow(refdata$fam)
+  ## Set the selection threshold for TWAS z-statistics
+  zthresh <- set_zthresh(ngenes, opts)
+  ## Return object
+  ret <- vector("list", length = nloci)
+
+  ## Loop over all loci
+  for (i in 1:nloci) {
+
+    ## Identify genes with expression data overlapping with the current locus
+    ge_keep <- genelist$P0 < cons_loci$ends[i] & genelist$P1 > cons_loci$starts[i]
+
+    ## Set up vars for gene selection
+    marg_z <- genelist$TWAS.Z   ## The original test statistics, preserved
+    cond_z <- genelist$TWAS.Z   ## Comb. adjusted test stat / processing flag
+    cond_z[ !ge_keep ] <- 0     ## ... 0 for genes not part of selection / conditioning
+    joint_keep <- rep(FALSE, ngenes) ## Indicator for final set of conditioning genes
+
+    ## Prepare header for output file, if required
+    if ( opts$report ) {
+      file.report <- paste0(opts$out, ".report")
+      cat( "FILE", "CHR", "P0", "P1", "HIT.GENES", "JOINT.GENES", "BEST.TWAS.P",
+           "BEST.SNP.P", "COND.SNP.P", "VAR.EXP\n", sep='\t' , file=file.report )
+    }
+
+    ## SKIP the locus if none of the original TWAS statistics is above the
+    ## specified threshold
+    if ( sum(cond_z^2 > zthresh^2) == 0 ) {
+      if( opts$verbose > 0 ) {
+        cat( "WARNING: no models in CLUMP ", i , " had an absolute marginal association statistic higher than " ,
+             zthresh , ". Skipping\n" , sep='' , file=stderr() )
+      }
+    ## Continue with gene selection
+    } else {
+      while ( any(cond_z^2 > zthresh^2) ) {
+
+        ## Add most conditionally significant feature
+        ndx_max <- which.max(cond_z^2)
+        joint_keep[ ndx_max ] <- TRUE
+        if( opts$verbose > 1 ) {
+          cat( (genelist$FILE)[ ndx_max ] , " added to model with conditional Z-score of ",
+               cond_z[ndx_max] , "\n" , sep='' , file=stderr() )
+        }
+
+        ## Invert correlation matrix of predicted gene expression values to for
+        ## local imputation of TWAS Z_statistics
+        ## FIXME: reference to paper / equation?
+        cur_dinv <- solve(pred_ge_corr[joint_keep, joint_keep])
+        ## Loop over genes still remaining in the selection set
+        for ( ii in which(cond_z != 0) ) {
+          ## Gene has been added to conditioning set, remove from selection set
+          if ( joint_keep[ii] ) {
+            cond_z[ii] <- 0
+          ## Gene has to too strong correlation with at least one gene in the
+          ## conditioning set, remove from selection set
+          } else if ( max( pred_ge_corr[ii, joint_keep]^2 ) > opts$max_r2 ) {
+            cond_z[ii] <- 0
+          ## Working branch: calculate the adjusted (conditional) TWAS statistic
+          ## for the current gene relative to the conditioning set, and update
+          ## the vector of conditional z-statistics
+          ## FIXME: reference to paper / equation?
+          } else {
+
+            pred_ge_corr_local <- pred_ge_corr[ii, joint_keep, drop=FALSE]
+            cur_b     <- marg_z[ii] - pred_ge_corr_local %*% cur_dinv %*%
+                                      marg_z[joint_keep, drop=FALSE]
+            cur_b_var <- 1 - pred_ge_corr_local %*% cur_dinv %*% t(pred_ge_corr_local)
+
+            ## Check: if we get negative variance, we drop the current gene from
+            ## the selection set (crude but effective way of dealing with presumed
+            ## collinearity)
+            if ( cur_b_var <= 0 ) {
+              cond_z[ii] <- 0
+            } else {
+              cond_z[ii] <- cur_b / sqrt( cur_b_var )
+            }
+
+          }  ## end of if
+        }    ## end of for loop over current selection set
+      }      ## end of while loop for genes with TWAS over threshold
+
+      ## We invert the correlation matrix of the final conditioning set to
+      ## prepare for adjusting the GWAS z-statistics
+      ## We add a stabilizing constant on the diagonal, similar to the original
+      ## imputation algorithm described by Gusev et al 2016 - this is hand-tuned
+      ## by the user (default: zero, no diagonal fortification)
+      nkeep <- length(which(joint_keep))
+      cur_dinv <- solve( pred_ge_corr[joint_keep, joint_keep] + lambda*diag(nkeep) )
+
+      ## Select SNPs to be retained in current locus
+      cur_keep_snp = refdata$bim[, "BP"] > cons_loci$starts[i] &
+                     refdata$bim[, "BP"] < cons_loci$ends[i]
+      snp_z  <- sumstat$Z[ cur_keep_snp ]
+      ## FIXME: why do we recalculate this?
+      snp_pv <- 2*(pnorm( abs(snp_z) , lower.tail=FALSE))
+
+      ## Covariance between observed SNPs and predicted gene expression across
+      ## reference subjects, resulting in SNP x gene matrix
+      ## Note that both contributing matrices are still scaled appropriately
+      ##
+      ## FIXME: reference?
+      snp_ge_ld = t(refdata$bed[, cur_keep_snp]) %*% pred_ge[, joint_keep] / ( nsubj - 1 )
+
+      ## TODO (optional) : impute any missing z-scores
+
+      ## Calculate adjusted GWAS z-stats & standard errors
+      ## FIXME: reference?
+      snp_cond_b   <- snp_z - snp_ge_ld %*% cur_dinv %*% marg_z[joint_keep]
+      snp_cond_var <- 1 - diag( snp_ge_ld %*%  cur_dinv  %*% t(snp_ge_ld) )
+      ## Check: invalid variances?
+      ndx_neg_var <- which(snp_cond_var <= 0)
+      if (any(ndx_neg_var)) {
+        if( opts$verbose > 0 ) {
+          cat( "WARNING: ", length(ndx_neg_var), " SNPs in CLUMP ", i ,
+               " had a negative variance for their adjusted test statistics.",
+               "Their statistics will be set to NaN\n", sep='' , file=stderr() )
+        }
+      }
+      snp_cond_se <- sqrt(snp_cond_var)
+
+      ## Zero out SNPs with > max_r2 LD to a gene
+      autocor <- apply(snp_ge_ld^2, 1, max, na.rm=TRUE) > opts$max_r2
+      if (any(autocor)) {
+        if( opts$verbose > 0 ) {
+          cat( "WARNING: ", length(which(autocor)), " SNPs in CLUMP ", i ,
+               " had squared correlation with predicted expression >", opts$max_r2,
+               ". Their statistics will be set to zero\n", sep='' , file=stderr() )
+        }
+      }
+      snp_cond_b[ autocor ]  <- 0
+      snp_cond_se[ autocor ] <- 1
+
+      ## Test statistic and p-value for adjusted test statistics
+      snp_cond_z <- snp_cond_b / snp_cond_se
+      snp_cond_p <- 2*(pnorm( abs(snp_cond_z) , lower.tail=FALSE))
+
+      ## Some summary stats for the current locus
+      ##if( opts$verbose > 0 ) {
+      ##  cat("Locus " , i , ":\n", sep='', file=stderr())
+      ##  cat( "\tbest GWAS Chisq\t" , round(max(snp_z^2, na.rm=TRUE), 2) , '\n' ,
+      ##       sep='', file=stderr())
+      ##  cat( "\tbest GWAS Chisq conditioned\t" , round(snp_cond_z[which.max(snp_z^2)]^2, 2),
+      ##       '\n' , sep='', file=stderr() )
+      ##  cat( "\tbest conditioned Chisq\t" , round(max(snp_cond_z^2, na.rm=TRUE), 2) ,
+      ##       '\n' , sep='', file=stderr() )
+      ##}
+
+      # generate before/after manhattan plot
+      # THIS IS WHERE THE OPT PLOT GOES
+
+      ## Create results for return (redundancy for possible reporting)
+      ## Start with results for SNPs
+      snp_res <- data.frame("SNP" = refdata$bim[cur_keep_snp, 2],
+                            "POS" = refdata$bim[cur_keep_snp, 4],
+                            "GWAS.Z" = snp_z , "GWAS.P" = snp_pv,
+                            "GWAS_cond.Z" = snp_cond_z, "GWAS_cond.P" = snp_cond_p)
+      ## FIXME: derive from snp_res
+      snp_res2 <- data.frame("SNP" = refdata$bim[cur_keep_snp,2],
+                             "POS" = refdata$bim[cur_keep_snp,4],
+                             "GWAS.LOGP" = -log10(snp_pv) ,
+                             "GWAS_cond.LOGP" = -log10(snp_cond_p) )
+      ## Gene results
+      gene_res <- genelist[ ge_keep, ]
+      gene_res$JOINT <- joint_keep[ ge_keep ]
+      ## Compute correlation of each gene to the top SNP
+      gene_res$TOP.SNP.COR <- round( t( pred_ge[, ge_keep] ) %*%
+                                      (refdata$bed[, cur_keep_snp])[, which.max(snp_z^2), drop=FALSE]/ (nsubj - 1 ), 2)
+
+      ret[[i]] <- list(snp_res = snp_res, gene_res = gene_res)
+
+      ## Conditional reporting
+      if ( opts$save_loci ) {
+        write.table( format(snp_res, cdigits=2), quote=FALSE, col.names=TRUE,
+                     row.names=F , sep='\t' , file=paste0(opts$out,".loc_",i,".cond") )
+      }
+      if ( opts$report ) {
+        ## SNP statistics for the current locus
+        write.table( format(snp_res2, digits=2, trim=TRUE) , quote=FALSE,
+                     col.names=TRUE, row.names=FALSE, sep=',' ,
+                     file=paste0(opts$out,".loc_",i,".cond.csv") )
+        ## Gene statistics for the current locus
+        write.table( gene_res, quote=FALSE , row.names=FALSE , col.names=TRUE ,
+                     sep='\t' , file=paste0(opts$out,".loc_", i, ".genes")  )
+        ## Summary statistics for the current locus
+        ## FIXME: build df incrementally, drop as file after loop
+        best.snp.chisq <- max(snp_z^2, na.rm=TRUE)
+        cond.snp.chisq <- snp_cond_z[which.max(snp_z^2)]^2
+        cat( paste(opts$out, ".loc_", i, sep=''), opts$chr,
+             range( refdata$bim[ cur_keep_snp , 4 ] ),
+             length(unique(genelist$ID[ge_keep])) , sum( joint_keep[ ge_keep ] ),
+             min(genelist$TWAS.P[ ge_keep ], na.rm=TRUE),
+             2*pnorm(sqrt(best.snp.chisq), lower.tail=FALSE),
+             2*pnorm(sqrt(cond.snp.chisq), lower.tail=FALSE),
+             1 - cond.snp.chisq / best.snp.chisq , '\n' , sep='\t' ,
+             file=file.report , append=TRUE)
+      }
+    } ## End of branch: are there sufficiently large TWAS z-stats to condition on?
+  } ## End of locus-loop
+
+  ret
+
+}
